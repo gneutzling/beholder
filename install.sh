7d7cc8bcf8657233bb6d675dcaab52f27a4ec25a
diogoviannaaraujo <diogoviannaaraujo@icloud.com>
2014-08-31 13:10:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 # Install Dependencies
 # -------------------------
 
-# Add node repository
-curl -sL https://deb.nodesource.com/setup | bash -
+# Downloading latest node
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 
 # Add gstreamer Repo
 echo "deb http://vontaene.de/raspbian-updates/ . main" >> /etc/apt/sources.list
 
 # Install Node and Gstreamer
 apt-get update
-apt-get install nodejs
+dpkg -i node_latest_armhf.deb
 apt-get install libgstreamer1.0-0-dbg gstreamer1.0-tools libgstreamer-plugins-base1.0-0 gstreamer1.0-plugins-good gstreamer1.0-plugins-bad-dbg gstreamer1.0-omx gstreamer1.0-alsa
