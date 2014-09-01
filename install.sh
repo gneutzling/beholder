d059af38491396539a869277b46b2c2c0a3e2791
Diogo Vianna <diogoviannaaraujo@icloud.com>
2014-08-31 21:24:50 -0300

    Adding node packages instalation

--- install.sh
+++ install.sh
@@ -14,3 +14,12 @@ echo "deb http://vontaene.de/raspbian-updates/ . main" >> /etc/apt/sources.list
 apt-get update
 dpkg -i node_latest_armhf.deb
 apt-get install libgstreamer1.0-0-dbg gstreamer1.0-tools libgstreamer-plugins-base1.0-0 gstreamer1.0-plugins-good gstreamer1.0-plugins-bad-dbg gstreamer1.0-omx gstreamer1.0-alsa
+
+# -------------------------
+# Install Npm Packages
+# -------------------------
+
+# This should be at package.json
+npm install -g express
+npm install -g net
+npm install -g child_process
