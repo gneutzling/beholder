4e77e706c5fdcb23c6f752f801299a3191dff47f
root <root@alarmpi.home>
2014-09-16 18:17:11 -0600

    major app rewrite and adding shript to start gstreamer pipeline

--- app.js
+++ app.js
@@ -1,56 +1,62 @@
 app = require('express')();
-net = require('net');
 child = require('child_process');
+io = require('socket.io').listen(app)
 
-app.get('/stream', function(req, res) {
-  res.writeHead(200, {
-    'Date': (new Date()).toUTCString(),
-    'Connection': 'close',
-    'Cache-Control': 'private',
-    'Content-Type': 'video/mp4'
-  });
-  server = net.createServer(function(socket) {
-    socket.on('data', function(data) {
-      console.log(data)
-      return res.write(data);
-    });
-    return socket.on('close', function(had_error) {
-      return res.end();
-    });
-  });
-  server.maxConnections = 1;
-  return server.listen(function() {
-    var args, gst_muxer;
-    //gst-launch-1.0 -e v4l2src device=/dev/video0 ! 'video/x-raw,width=640,height=480,framerate=30/1' ! omxh264enc ! avimux name=mux ! filesink location=webcam3.avi alsasrc device=hw:1,0 ! audioconvert ! 'audio/x-raw,rate=44100,channels=2' ! mux.
-    //gst-launch-1.0 -e v4l2src device=/dev/video0 ! 'video/x-raw,width=640,height=480,framerate=30/1' ! omxh264enc ! avimux ! filesink location=webcam2.avi
-    //gst-launch-1.0 -e v4l2src device=/dev/video0 ! 'video/x-raw,width=640,height=480,framerate=30/1' ! omxh264enc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpclientsink host=localhost port=1000
-    //h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=0.0.0.0 port=5000
-    //args = ['v4l2src', '!', 'video/x-raw,width=640,height=480,framerate=15/1', '!', 'h264enc bitrate=1000', '!', 'video/x-h264,profile=high', '!', 'h264parse', '!', 'queue', '!', 'flvmux name=mux alsasrc device=hw:1', '!', 'audioresample', '!', 'audio/x-raw,rate=48000', '!', 'queue', '!', 'voaacenc', 'bitrate=32000', '!', 'queue', '!', 'mux. mux.', '!', 'tcpclientsink', 'host=localhost', 'port=' + server.address().port];
-    args = ['-e', 'v4l2src device=/dev/video0', '!', 'video/x-raw,width=640,height=480,framerate=30/1', '!', 'omxh264enc', '!', 'rtph264pay pt=96', '!', 'tcpclientsink', 'host=localhost', 'port='+server.address().port]
-    console.log(server.address().port);
-    gst_muxer = child.spawn('gst-launch-1.0', args, null);
-    gst_muxer.stderr.on('data', onSpawnError);
-    gst_muxer.on('exit', onSpawnExit);
-    return res.connection.on('close', function() {
-      return gst_muxer.kill();
-    });
-  });
-});
+// Main configuration for app
+var config = {
+  'mothership':	    'http://live.taller.net.br/',
+  'cameraID':       'casadiogo'
+}
 
-app.listen(7777);
-
-onSpawnError = function(data) {
-  return console.log(data.toString());
-};
-
-onSpawnExit = function(code) {
-  if (code != null) {
-    return console.error('GStreamer error, exit code ' + code);
-  }
-};
-
-process.on('uncaughtException', function(err) {
-  return console.log(err);
-});
+// Keeps Application State
+var state = {
+  'current':        'idle',
+  'connection':     'disconnected'
+  'alarmMovement':  False
+  'recording':      False
+}
 
 
+// Init Function
+// Here we will start connection with mothership 
+init = function() {
+  connectMS();
+}
+
+
+// Execute Socket Connection to mothership
+// All socket io implementation will be done here, function calling auto reconnection
+connectMS = function() {
+  state.connection = 'connecting';
+  // Open socket to mothershio
+  state.connection = 'connected'
+  // on error, disconnect and etc loop?
+}
+
+// Handle streaming to Mothership
+// Will start gstreamer pipeline and kill gsteramer process when needed
+startSteaming = function() {
+  // Start gstreamer
+}
+
+stopSteaming = function() {
+  // Kills gsteamer process
+}
+
+// Handle monitoring
+// Will check for movement using opencv and release the camera when asked
+startMonitoring = function {
+  state.current = 'monitoring';
+  // uses opencv to grab camera and check for movement
+}
+
+stopMonitoring = function {
+  //releases opencv camera and stuff
+  state.current = 'idle';
+}
+
+// Function called when there is movement and alarm is set
+alarmTrigger = function() {
+  // Execute alarm procedure
+  // Notify MS throu socket
+}
